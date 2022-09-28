252e8117017a3d5547f8898817b7db2ae9a37a34
ivan2798 <inge1998@hotmail.com>
2022-09-28 12:22:56 -0500

    String Methods

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 #Ask user for their name
-name = input("Whast's your name?")
+#Remove whitespace from str and capitalize user's name and 
+name = input("Whast's your name?").strip().title()
+
+#Split user's name into first name and last name
+first, last = name.split(" ")
 
 #Say hello to user
-print("hello,", name)
+print(f"hello, {first} ")
+
